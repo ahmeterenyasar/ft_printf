cdb5ad6297f86c4d7da240f4cd68a9caeb81e269
Ahmet Eren Yasar <ahmet.eren.yasar@hotmail.com>
2026-01-30 22:27:34 +0300

    feat: header update, functiosn added, ft_print_nums completed

---
+++ ft_print_nums.c
@@ -0,0 +1,42 @@
+
+#include "ft_printf.h"
+
+int ft_print_nbr(int n)
+{
+    int len;
+    char *str;
+
+    len = 0;
+    if (n == -2147483648)
+    {
+        write(1, "-2147483648", 11);
+        return (11);
+    }
+    if (n < 0)
+    {
+        len += ft_print_char('-');
+        n = -n;
+    }
+    if (n > 9)
+    {
+        len += ft_print_nbr(n / 10);
+        len += ft_print_nbr(n % 10);
+    }
+    else
+        len += ft_print_char(n + '0');
+    return len;
+}
+
+int ft_print_unsigned(unsigned int n)
+{
+    int len;
+
+    len = 0;
+    if (n >= 10)
+    {
+        len += ft_print_unsigned(n / 10);
+    }
+    len += ft_print_char((n%10) + '0');
+    return len;
+    
+}
--- ft_printf.h
+++ ft_printf.h
@@ -4,6 +4,13 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-
+int		ft_printf(const char *format, ...);
+int		ft_print_char(int c);
+int		ft_print_str(char *str);
+int		ft_print_percent(void);
+int		ft_print_nbr(int n);
+int		ft_print_unsigned(unsigned int n);
+int		ft_print_hex(unsigned int n, const char format);
+int		ft_print_ptr(unsigned long long ptr);
 
 #endif
